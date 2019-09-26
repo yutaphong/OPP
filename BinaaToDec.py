9f2c5d550f18cdff883819db072264812ffcc61c
yutaphong <55840818+yutaphong@users.noreply.github.com>
2019-09-26 15:35:09 +0100

    Add files via upload

---
+++ BinaaToDec.py
@@ -0,0 +1,10 @@
+# Python Program - Convert Binary to Decimal
+
+#user input
+binary = input("Enter number in Binary Format: ");
+
+#convert
+decimal = int(binary, 2);
+
+#display
+print(binary,"in Decimal =",decimal);
